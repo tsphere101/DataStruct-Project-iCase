8af722e5aaca3e27ad00168050039c426f212e97
Krissada Sarawit <topfeekrissada@outlook.com>
2021-11-28 06:41:32 +0700

    update urls and pages

--- pages/views.py
+++ pages/views.py
@@ -3,11 +3,17 @@ from django.shortcuts import render
 
 # Create your views here.
 
-def home_view(request,*args,**kwargs):
-    return render(request,"main.html",{})
+def home_view(request, *args, **kwargs):
+    return render(request, "main.html", {})
 
-def cases_view(request,*args,**kwargs):
-    return render(request,"cases.html",{})
 
-def collection_view(request,*args,**kwargs):
-    return render(request,"collection.html",{})
+def cases_view(request, *args, **kwargs):
+    return render(request, "cases.html", {})
+
+
+def collection_view(request, *args, **kwargs):
+    return render(request, "collection.html", {})
+
+
+def test_view(request):
+    return render(request, 'base.html')
