3bda47affef899552de22324cfc94e980b667b7a
Krissada Sarawit <topfeekrissada@outlook.com>
2021-11-28 06:42:07 +0700

    update urls

--- iCaseProject/urls.py
+++ iCaseProject/urls.py
@@ -5,9 +5,11 @@ import pages.views as page
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("",page.home_view,name='home-view'),
-    path("home/",page.home_view),
-    # path("cases/",page.cases_view),
     path('cases/',include('product.urls')),
+
+    # path("cases/",page.cases_view),
     # path("collection/",page.collection_view),
+
+    path('test/',page.test_view),
 
 ]
