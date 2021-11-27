c495c5abe348f553978e5ac01739ba0dfadc510d
Krissada Sarawit <topfeekrissada@outlook.com>
2021-11-28 05:53:15 +0700

    Re-routing urls and pages

--- pages/views.py
+++ pages/views.py
@@ -1,6 +1,13 @@
 from django.shortcuts import render
 
+
 # Create your views here.
 
 def home_view(request,*args,**kwargs):
-    return render(request,"index.html",{})
+    return render(request,"main.html",{})
+
+def cases_view(request,*args,**kwargs):
+    return render(request,"cases.html",{})
+
+def collection_view(request,*args,**kwargs):
+    return render(request,"collection.html",{})
