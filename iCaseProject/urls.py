e40fd252d316bfc98d513cd1f66237d95254f975
Krissada Sarawit <topfeekrissada@outlook.com>
2021-11-28 12:03:31 +0700

    Added MEDIA_URL path

--- iCaseProject/urls.py
+++ iCaseProject/urls.py
@@ -2,6 +2,9 @@ from django.contrib import admin
 from django.urls import path,include
 import pages.views as page
 
+from django.conf import settings
+from django.conf.urls.static import static
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("",page.home_view,name='home-view'),
@@ -12,4 +15,4 @@ urlpatterns = [
 
     path('test/',page.test_view),
 
-]
+]+ static(settings.MEDIA_URL,document_root = settings.MEDIA_ROOT)
