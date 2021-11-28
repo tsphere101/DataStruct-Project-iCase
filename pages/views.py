4ce92925a74e42855462e04a11cd77ea3be868c5
Krissada Sarawit <topfeekrissada@outlook.com>
2021-11-28 08:24:50 +0700

    Remove unused functions

--- pages/views.py
+++ pages/views.py
@@ -6,14 +6,5 @@ from django.shortcuts import render
 def home_view(request, *args, **kwargs):
     return render(request, "main.html", {})
 
-
-def cases_view(request, *args, **kwargs):
-    return render(request, "cases.html", {})
-
-
-def collection_view(request, *args, **kwargs):
-    return render(request, "collection.html", {})
-
-
 def test_view(request):
     return render(request, 'base.html')
